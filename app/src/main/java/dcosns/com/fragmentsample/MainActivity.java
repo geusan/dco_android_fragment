9aec00f8e113a931d6e4c2dc668e3a47af40dda1
geusan <dnay2k@naver.com>
2016-08-19 14:04:23 +0900

    Add Comment  for you

--- app/src/main/java/dcosns/com/fragmentsample/MainActivity.java
+++ app/src/main/java/dcosns/com/fragmentsample/MainActivity.java
@@ -6,12 +6,17 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity/*프래그먼트의 부모는 FragmentActivity로 만들어야한다.*/ {
 
-    LinearLayout frame;
+
     Button[] btns = new Button[3];
+
+    /**
+     * 한번에 버튼기능을 관리하기 위해
+     *
+     * 알아보기 쉽도록 메소드로 처리한다.
+     */
     View.OnClickListener bHandler = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -30,6 +35,11 @@ public class MainActivity extends FragmentActivity {
         }
     };
 
+    /**
+     * 식별자에 따라서 다른 Fragment를 호출해오는 메소드
+     * @param position 식별자, 코드간소화를 하기 위해 버튼 배열과 같은 값을 사용
+     * @return postition에 따라서 다른 프래그먼트가 반환된다.
+     */
     private Fragment getFragmnet(int position){
         Fragment f= null;
         switch (position){
@@ -40,15 +50,25 @@ public class MainActivity extends FragmentActivity {
         return f;
     }
 
+    /**
+     * 누른 버튼에 따라서 다른 화면이 나오도록 한다.
+     *
+     * @param position 누른 버튼에 대한 식별자
+     */
     private void changeView(int position){
         btns[position].setBackgroundResource(R.drawable.btn_clicked);
         Fragment f = getFragmnet(position);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frame, f).commit();
     }
+
+    /**
+     * 버튼눌렀을 때마다 호출되는 초기화 메소드
+     */
     private void initBg(){
         for(Button b : btns) b.setBackgroundResource(R.drawable.btn_unclicked);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/dcosns/com/fragmentsample/child1.java
+++ app/src/main/java/dcosns/com/fragmentsample/child1.java
@@ -13,6 +13,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -21,11 +22,11 @@ import org.json.JSONObject;
 /**
  * Created by dnay2 on 2016-08-17.
  */
-public class child1 extends Fragment {
+public class child1 extends Fragment/* Fragment를 상속받는다.*/ {
 
     Button btn;
     EditText text;
-    RequestQueue volley;
+    RequestQueue volley; //요청을 담는 큐
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -46,18 +47,27 @@ public class child1 extends Fragment {
     private void doAction(){
         String url = "http://apis.skplanetx.com/melon/charts/realtime?count=10&page=1&version=1&appKey=31c1e579-b9c2-3697-95d3-ed1ba8a82815&format=json";
 
-//        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                text.setText(response);
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//
-//            }
-//        });
+        /**
+         * Http 요청에 대한 응답을 String형태로 받아서 사용하는 방법
+         */
+        StringRequest request1 = new StringRequest(url/*요청 url*/,
+                new Response.Listener<String>()/*응답받을 때 사용하는 리스너(인터페이스)*/ {
+            @Override
+            public void onResponse(String response) {
+                text.setText(response);
+            }
+        }, new Response.ErrorListener()/*에러가 발생했을 때 사용하는 리스너(인터페이스)*/ {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //에러가 발생했을 때 안내창을 띄우는 형식의 반응을 보여주면 된다.
+            }
+        });
 
+        /**
+         *  사용하는 방법은 위의 StringRequest와 동일하다 대신에 받아오는 응답을
+         *
+         *  JSON으로 받아오기때문에 파싱이 더 쉬울 수 있다.
+         */
         JsonObjectRequest request = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -83,6 +93,11 @@ public class child1 extends Fragment {
             }
         });
 
+        /**
+         * 마지막에 RequestQueue 에 요청을 추가해주어야한다.
+         *
+         * RequestQueue에 담으면 담긴 순서대로 요청을 수행한다.
+         */
         volley.add(request);
     }
 
